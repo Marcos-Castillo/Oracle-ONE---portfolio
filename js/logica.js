92ecdc3e77a531a54f6be56103f77664546d576c
Marcos-Castillo <marcos469204@gmail.com>
2022-03-01 04:14:21 -0300

    finalizado challenge 1 y 2

---
+++ js/logica.js
@@ -0,0 +1,9 @@
+const convertPdf = document.querySelector("#convertPdf");
+convertPdf.addEventListener("click", function () {
+
+//   var source = window.document.getElementsByTagName("body");
+//   var doc = new jsPDF();
+//   doc.fromHTML(source);
+//   doc.output("dataurlnewwindow");
+print()
+});
